63f970025ea6e2f67793501c34bff5f8a55ed388
Mike Sherov <mike.sherov@gmail.com>
2013-12-06 17:52:41 -0500

    Trailing Comments: make sure a closure assignment has a semicolon

--- Behance/Sniffs/Comments/TrailingCommentSniff.php
+++ Behance/Sniffs/Comments/TrailingCommentSniff.php
@@ -98,12 +98,23 @@ class Behance_Sniffs_Comments_TrailingCommentSniff implements PHP_CodeSniffer_Sn
 
     } // if there is whitespace right after curly brace
 
+    // make sure assignment closures have a semicolon
+    if ( $this->_isCloseOfAnAssignedAnonymousFunction( $tokens, $stackPtr, $phpcsFile ) ) {
+      if ( !isset( $tokens[ $stackPtr + 1 ] ) || $tokens[ $stackPtr + 1 ]['code'] !== T_SEMICOLON ) {
+        $phpcsFile->addError( 'semicolon not found after anonymous function assignment', $stackPtr + 1 );
+
+        return;
+      } // if !semicolon
+
+      $stackPtr++;
+    } // if _isCloseOfAnAssignedAnonymousFunction
+
     // get the comment
     $commentPtr = $stackPtr + 2;
 
     if ( !isset( $tokens[ $commentPtr ] ) || $tokens[ $commentPtr ]['type'] !== 'T_COMMENT' ) {
 
-      $phpcsFile->addError( 'Disallowed use of scope operator detected', $stackPtr );
+      $phpcsFile->addError( 'trailing comment not found after closing curly', $stackPtr );
 
       return;
 
@@ -121,6 +132,54 @@ class Behance_Sniffs_Comments_TrailingCommentSniff implements PHP_CodeSniffer_Sn
     } // if empty comment or missing whitespace
 
   } // process
+
+  /**
+   * closures need a semicolon before the trailing comment, but only when it's an assignment
+   * so look for this situation:
+   * 1. find the opening curly, 2. see if it's an anonymous function, 3. see if it's being assigned!
+   *
+   * @param   array $tokens
+   * @param   int   $stackPtr
+   * @param   PHP_CodeSniffer_File $phpcsFile
+   * @return  boolean
+   */
+  protected function _isCloseOfAnAssignedAnonymousFunction ( $tokens, $stackPtr, PHP_CodeSniffer_File $phpcsFile ) {
+
+    // 1. find the opening curly
+    $curly_depth = 1;
+    $prevToken = $stackPtr;
+    while ( ( $prevToken = $phpcsFile->findPrevious( [ T_CLOSE_CURLY_BRACKET, T_OPEN_CURLY_BRACKET ], $prevToken - 1 ) ) !== false ) {
+      if ( $tokens[$prevToken]['code'] === T_OPEN_CURLY_BRACKET ) {
+        $curly_depth--;
+      }
+      else {
+        $curly_depth++;
+      }
+
+      if ( $curly_depth === 0 ) {
+        break;
+      }
+    } // while prevToken
+
+    if ( $prevToken === false ) {
+      return false;
+    }
+
+    $curlyOpenerPtr = $phpcsFile->findPrevious( PHP_CodeSniffer_Tokens::$scopeOpeners, $prevToken - 1 );
+
+    if ( $curlyOpenerPtr === false || $tokens[$curlyOpenerPtr]['type'] !== 'T_CLOSURE' ) {
+      return false;
+    }
+
+
+    $assignmentPtr = $phpcsFile->findPrevious( PHP_CodeSniffer_Tokens::$emptyTokens, $curlyOpenerPtr - 1, null, true );
+    if ( $assignmentPtr === false ) {
+      return false;
+    }
+
+    return in_array( $tokens[$assignmentPtr]['code'], PHP_CodeSniffer_Tokens::$assignmentTokens );
+
+  } // _isCloseOfAnAssignedAnonymousFunction
 
   /**
    * Starts from the *end* of the scope (ie: where '}' is)
