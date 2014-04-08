4e09eee4c9ce6eb998b171608daa816ce704dc52
Mike Sherov <mike.sherov@gmail.com>
2014-04-07 21:34:21 -0400

    Function Call Spacing: 100% Code Coverage

--- tests/Behance/Sniffs/Functions/FunctionCallArgumentSpacingSniffTest.php
+++ tests/Behance/Sniffs/Functions/FunctionCallArgumentSpacingSniffTest.php
@@ -5,9 +5,13 @@ class Behance_Sniffs_Functions_FunctionCallArgumentSpacingSniffTest extends Abst
   public function getErrorList() {
 
     return [
-        2 => 1,
-        3 => 1,
-        6 => 1
+        2  => 1,
+        3  => 1,
+        6  => 1,
+        10 => 1,
+        13 => 1,
+        15 => 1,
+        18 => 2
     ];
 
   } // getErrorList
