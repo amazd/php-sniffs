4f91bdbad40f0afd25c9d3a0b3014c1aa8e69515
Mike Sherov <mike.sherov@gmail.com>
2014-04-08 19:29:36 -0400

    Array Bracket Spacing: 100% Code Coverage

---
+++ tests/Behance/Sniffs/Arrays/ArrayBracketSpacingSniffTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class Behance_Sniffs_Arrays_ArrayBracketSpacingSniffTest extends AbstractSniffUnitTest {
+
+  public function getErrorList() {
+
+    return [
+        4 => 2,
+    ];
+
+  } // getErrorList
+
+  public function getWarningList() {
+
+    return [];
+
+  } // getWarningList
+
+} // Behance_Sniffs_Arrays_ArrayBracketSpacingSniffTest
